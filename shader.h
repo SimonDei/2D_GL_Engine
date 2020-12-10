1b3d96c5730b643f37e7fcf00ce2c96531c3faef
SimonDei <s.f.deitermann@gmail.com>
2020-12-10 15:07:14 +0100

    Update shader.h

--- shader.h
+++ shader.h
@@ -8,11 +8,14 @@
 #include "macros.h"
 #include "types.h"
 #include "results.h"
+#include "math.h"
 
 typedef struct _SHADER {
     XD_UINT32 _program_id;
+    XD_UINT32 _vertex_array_id;
     XD_UINT32 _vertex_shader_id;
     XD_UINT32 _fragment_shader_id;
+    XD_UINT32 _indicies_count;
     XD_UINT16 _buffer_index;
     XD_UINT32 _buffers[1024];
 } XD_SHADER;
@@ -22,7 +25,28 @@ EXPORT XD_RESULT create_shader(
     _In_ XD_STRING fragment_shader_path,
     _Out_ PTR(XD_SHADER) p_shader);
 
+EXPORT XD_RESULT set_vertex_attrib(
+    _In_ PTR(XD_SHADER) p_shader,
+    _In_ XD_UINT8 position,
+    _In_ XD_UINT8 vertex_size,
+    _In_ PTR(XD_FLOAT) p_data,
+    _In_ XD_UINT32 data_size);
+
+EXPORT XD_RESULT set_indicies(
+    _In_ PTR(XD_SHADER) p_shader,
+    _In_ XD_UINT8 vertex_size,
+    _In_ PTR(XD_UINT32) p_data,
+    _In_ XD_UINT32 data_size);
+
+EXPORT XD_RESULT set_uniform(
+    _In_ XD_SHADER shader,
+    _In_ XD_STRING name,
+    _In_ XD_MATRIX4 matrix);
+
+EXPORT XD_RESULT execute_shader(
+    _In_ XD_SHADER shader);
+
 EXPORT XD_RESULT destroy_shader(
-    _In_ PTR(XD_SHADER) p_shader);
+    _In_ XD_SHADER shader);
 
 #endif //UNTITLED_SHADER_H
